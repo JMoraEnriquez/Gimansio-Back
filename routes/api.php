fda06cfaf1d4e157a7ae019720b9b64a92c7c9fc
JMoraEnriquez <jmoraenriquez1@gmail.com>
2022-09-05 19:21:20 -0500

    cositas...

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\api\prueba;
-use Illuminate\Http\Request;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::prefix('/user')->group(function(){
+
+    Route::post('/store', [UserController::class, 'store']);
+    Route::post('/login', [LoginController::class, 'login']);
+    //Route::get('/index', [UserController::class, 'index']);
+    Route::middleware('auth:api')->get('/index', [UserController::class, 'index']);
 });
 
 Route::get('/holaMundo', [prueba::class, 'holaMundo'] );
